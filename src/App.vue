4a98fd9c1e41e67dfc13f58453ff2f43df927ccd
Ezequiel Dominguez <ezequiel.dominguez@axlot.com>
2023-09-18 12:37:00 -0300

    Board: add initial structure data, random initial autofill (wip)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,74 @@
 <template>
-  <main></main>
+  <main>
+    <Board :board="board" />
+    <UserControls />
+  </main>
 </template>
 
-<script></script>
+<script>
+import Board from "@components/Board/Board.vue";
+import UserControls from "@components/UserControls/UserControls.vue";
+
+export default {
+  components: {
+    Board,
+    UserControls,
+  },
+
+  data() {
+    return {
+      boardSize: 9,
+      board: [],
+    };
+  },
+
+  methods: {
+    initBoard() {
+      // Paso 1: Generate an empty 9x9 board
+
+      const newBoard = new Array(this.boardSize)
+        .fill(null)
+        .map(() => new Array(this.boardSize).fill(0));
+
+      this.board = newBoard;
+      this.fillDiagonalRegions();
+    },
+
+    fillDiagonalRegions() {
+      for (let i = 0; i < this.boardSize; i += 3) {
+        this.fillRegion(i, i);
+      }
+    },
+
+    fillRegion(rowStart, colStart) {
+      const numbers = this.shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      let index = 0;
+      for (let i = rowStart; i < rowStart + 3; i++) {
+        for (let j = colStart; j < colStart + 3; j++) {
+          this.board[i][j] = numbers[index++];
+        }
+      }
+    },
+
+    // Paso 5: Función para mezclar un arreglo aleatoriamente
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
+  },
+
+  mounted() {
+    this.initBoard();
+  },
+};
+</script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+main {
+  display: flex;
+  gap: 1rem;
 }
 </style>
---
+++ src/components/Board/Board.vue
@@ -0,0 +1,32 @@
+<template>
+  <section id="board">
+    <template v-for="(row, rowIndex) in board" :key="rowIndex + '-row'">
+      <template
+        v-for="(rowItem, columnIndex) in row"
+        :key="columnIndex + '-column'"
+      >
+        <div>{{ rowItem }}</div>
+      </template>
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Board",
+  props: {
+    board: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#board {
+  display: grid;
+  grid-template-rows: repeat(9, 1fr);
+  grid-template-columns: repeat(9, 1fr);
+  gap: 1rem;
+}
+</style>
